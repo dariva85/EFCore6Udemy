a0c03e132ee69c4530b823b931195e73f4156575
David <david.rigalvall@gmail.com>
2023-03-16 11:35:42 +0100

    38.Obteniendo el Primer Registro con First y FirstOrDefault

--- EFCorePeliculas/EFCorePeliculas/Controllers/GenerosController.cs
+++ EFCorePeliculas/EFCorePeliculas/Controllers/GenerosController.cs
@@ -18,7 +18,31 @@ namespace EFCorePeliculas.Controllers
         [HttpGet]
         public async Task<IEnumerable<Genero>> Get()
         {
-            return await context.Generos.AsNoTracking().ToListAsync();
+            return await context.Generos.ToListAsync();
+        }
+
+        [HttpGet("primer")]
+        public async Task<ActionResult<Genero>> Primer()
+        {
+            var genero = await context.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("z"));
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+            return genero;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Primer(int id)
+        {
+            var genero = await context.Generos.FirstOrDefaultAsync(g => g.Identificador == id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+            return genero;
         }
     }
 }

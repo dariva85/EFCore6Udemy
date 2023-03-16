761144af632981877a0f8e9f2413a4ee5cae2186
David <david.rigalvall@gmail.com>
2023-03-16 17:25:57 +0100

    42. Seleccionando Columnas con Select (before DTO)

---
+++ EFCorePeliculas/EFCorePeliculas/Controllers/ActoresController.cs
@@ -0,0 +1,27 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController:ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var actores = await context.Actores
+                .Select(a=> new {id = a.Id, Nombre = a.Nombre})
+                .ToListAsync();
+            return Ok(actores);
+        }
+    }
+}

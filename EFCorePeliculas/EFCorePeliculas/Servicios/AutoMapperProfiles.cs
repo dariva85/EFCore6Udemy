a7daf55729bc8c86bfdfef909f86a72d1d2c945b
David <david.rigalvall@gmail.com>
2023-03-17 11:45:36 +0100

    44.Consultando datos espaciales

--- EFCorePeliculas/EFCorePeliculas/Servicios/AutoMapperProfiles.cs
+++ EFCorePeliculas/EFCorePeliculas/Servicios/AutoMapperProfiles.cs
@@ -9,6 +9,9 @@ namespace EFCorePeliculas.Servicios
         public AutoMapperProfiles()
         {
             CreateMap<Actor, ActorDTO>();
+            CreateMap<Cine, CineDTO>()
+                .ForMember(dto => dto.Latitud, ent => ent.MapFrom(prop => prop.Ubicacion.Y))
+                .ForMember(dto => dto.Longitud, ent => ent.MapFrom(prop => prop.Ubicacion.X));
         }
     }
 }

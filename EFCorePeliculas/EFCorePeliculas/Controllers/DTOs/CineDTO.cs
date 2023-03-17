ae782ec2740f6587cd4ef6ecbf52464016b0dbc1
David <david.rigalvall@gmail.com>
2023-03-17 13:19:11 +0100

    45.Ordenando y filtrando Datos Espaciales

---
+++ EFCorePeliculas/EFCorePeliculas/Controllers/DTOs/CineDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCorePeliculas.Controllers.DTOs
+{
+    public class CineDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+    }
+}

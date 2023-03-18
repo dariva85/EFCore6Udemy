fc3c8b31f63aa279f2e3c61ff136bafa82a923ff
Dariva85 <david.rigalvall@gmail.com>
2023-03-18 11:40:13 +0100

    47.Utilizando ThenInclude

---
+++ EFCorePeliculas/EFCorePeliculas/Controllers/DTOs/GeneroDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCorePeliculas.Controllers.DTOs
+{
+    public class GeneroDTO
+    {
+        public int Identificador { get; set; }
+        public string Nombre { get; set; }
+    }
+}

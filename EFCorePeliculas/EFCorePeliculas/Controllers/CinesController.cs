502300462ac7a3abb5a7c765115bb743017cf069
Dariva85 <david.rigalvall@gmail.com>
2023-03-19 10:20:35 +0100

    61. Insertar Registros con Data Relacionada

--- EFCorePeliculas/EFCorePeliculas/Controllers/CinesController.cs
+++ EFCorePeliculas/EFCorePeliculas/Controllers/CinesController.cs
@@ -46,5 +46,40 @@ namespace EFCorePeliculas.Controllers
                 .ToListAsync();
             return Ok(cines);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+            var ubicacionCine = geometryFactory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(-69.896979, 18.476276));
+
+            var cine = new Cine()
+            {
+                Nombre = "Mi Cine",
+                Ubicacion = ubicacionCine,
+                CineOferta = new CineOferta()
+                {
+                    PorcentajeDescuento = 5,
+                    FechaInicio = DateTime.Today,
+                    FechaFin = DateTime.Today.AddDays(7)
+                },
+                SalasDeCine = new HashSet<SalaDeCine>()
+                {
+                    new SalaDeCine()
+                    {
+                        TipoSalaDeCine = TipoSalaDeCine.DosDimensiones,
+                        Precio = 200
+                    },
+                    new SalaDeCine()
+                    {
+                        TipoSalaDeCine = TipoSalaDeCine.TresDimensiones,
+                        Precio = 350
+                    }
+                }
+            };
+            context.Add(cine);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }

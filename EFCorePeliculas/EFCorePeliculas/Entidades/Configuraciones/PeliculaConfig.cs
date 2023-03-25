1c6720706e4169e06527851a180ca61eef4c7b4b
Dariva85 <david.rigalvall@gmail.com>
2023-03-25 16:44:00 +0100

    93.Relación Muchos a Muchos con API Fluente - Sin Clase Intermedia

--- EFCorePeliculas/EFCorePeliculas/Entidades/Configuraciones/PeliculaConfig.cs
+++ EFCorePeliculas/EFCorePeliculas/Entidades/Configuraciones/PeliculaConfig.cs
@@ -17,6 +17,13 @@ namespace EFCorePeliculas.Entidades.Configuraciones
             builder.Property(prop => prop.PosterURL)
                 .HasMaxLength(500)
                 .IsUnicode(false);
+
+            //builder.HasMany(p => p.Generos)
+            //    .WithMany(g => g.Peliculas)
+            //    //Esto es por si quisieramos crear nuestra propia tabla intermedia con nuestros nombres
+            //    .UsingEntity(j => j.ToTable("PeliculasGeneros")
+            //            .HasData(new { PeliculasId = 1, GenerosIdentificador = 7 })
+            //            );
         }
     }
 }
